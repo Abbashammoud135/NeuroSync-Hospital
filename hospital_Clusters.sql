bceeb508064bb92af02843e3886a25e568cdc0e8
Abbas hammoud <125873580+Abbashammoud135@users.noreply.github.com>
2026-02-04 12:45:46 +0200

    Comment out clustered index creation and adjust nonclustered indexes

--- hospital_Clusters.sql
+++ hospital_Clusters.sql
@@ -1,4 +1,4 @@
---not run this command
+--
 -- CREATE CLUSTERED INDEX idx_Appointment_Date
 -- ON Appointment (appointment_datetime);
 
@@ -9,4 +9,5 @@ CREATE NONCLUSTERED INDEX idx_Doctor_Specialization
 ON Doctor (specialization);
 
 CREATE NONCLUSTERED INDEX idx_Billing_Patient
+
 ON Billing (patient_id);
